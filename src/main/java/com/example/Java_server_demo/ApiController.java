0d895df2f60e8a377aea5fdab3c7caf7bf2544bb
Charan Yeddula <120687005+Charanyeddula@users.noreply.github.com>
2024-07-28 19:28:33 -0700

    Update ApiController.java
    Update String to ResponseEntity Class

--- src/main/java/com/example/Java_server_demo/ApiController.java
+++ src/main/java/com/example/Java_server_demo/ApiController.java
@@ -11,27 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @GetMapping("/endpoint1")
-    public String endpoint1() {
-        return "Endpoint 1: 200:OK";
+    public ResponseEntity<String> endpoint1() {
+        return ResponseEntity.ok("Success 200: OK Endpoint 1");
     }
 
     @GetMapping("/endpoint2")
-    public String endpoint2() {
-        return "Endpoint 2: 200:OK";
+    public ResponseEntity<String> endpoint2() {
+        return ResponseEntity.ok("Success 200: OK Endpoint 2");
     }
 
     @GetMapping("/endpoint3")
-    public String endpoint3() {
-        return "Endpoint 3: 200:OK";
+    public ResponseEntity<String> endpoint3() {
+        return ResponseEntity.ok("Success 200: OK Endpoint 3");
     }
 
     @GetMapping("/endpoint4")
-    public String endpoint4() {
-        return "Endpoint 4: 404:Not Found";
+    public ResponseEntity<String> endpoint4() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
     }
 
     @GetMapping("/endpoint5")
-    public String endpoint5() {
-        return "Server Error. Please connect again later.";
+    public ResponseEntity<String> endpoint5() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server Error");
     }
 }
+
